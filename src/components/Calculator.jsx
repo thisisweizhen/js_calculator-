89bd0a69c69effbeb92a959c54ed889573e7ebc2
thisisweizhen <wei.zhen.suen@gmail.com>
2022-07-13 22:35:45 +0800

    new commit

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Calculator = () => {
+    return (
+        <div>
+            <div className ='container'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <h1 className='display-6 fw-bolder text-center text-primary'></h1>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Calculator;
